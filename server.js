becd79bf2c56dfc081b6188de9931e6b79c1383a
Samirtchak <mtcha015@uottawa.ca>
2020-06-13 15:39:55 -0400

    added get message service feature

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var app =express()
 
 app.use(express.static(__dirname))
 
+var messages = [
+    {name: 'Tim', message:'Hi'},
+    {name:'Horton', message:'Hello'}
+
+]
+
+
+app.get('/messages', (req,res) => {
+    
+    res.send(messages)
+})
+
 app.listen(3000, () => {
     console.log("server is listening on port 3000")
 })
+
+
