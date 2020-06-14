5a0f0c8165d09c26873c186cfb48a1683cb580b8
Samirtchak <mtcha015@uottawa.ca>
2020-06-13 21:16:39 -0400

    added the ability to post a message using the send button

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 
 var express = require('express')
 var app =express()
+var bodyParser= require('body-parser')
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json()) // to convert the data received in json format
+app.use(bodyParser.urlencoded({extended:false}))
 
 var messages = [
     {name: 'Tim', message:'Hi'},
@@ -10,10 +13,16 @@ var messages = [
 
 ]
 
-
+// get service
 app.get('/messages', (req,res) => {
     
     res.send(messages)
+})
+
+// post service
+app.post('/messages', (req,res) => {
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 app.listen(3000, () => {
